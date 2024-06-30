f70673094cde355e6ed3bc7fb543980c03bf5467
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-06-30 20:32:52 +0100

    💄 style: improve style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import { CommandBar } from "~/components/CommandBar";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-white to-slate-400 text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-white px-4 py-16">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        <h1 className="text-2xl font-bold tracking-tight sm:text-[5rem]">
+          git fetch all-users
         </h1>
-        <div className="w-1/2">
+        <div className="w-full max-w-2xl">
         <CommandBar />
         </div>
       </div>
