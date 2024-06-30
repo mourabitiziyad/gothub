90c5efdce972c58713ebd29a72bcbce60e0544ba
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-07-01 00:00:25 +0100

    🚑 fix(types): fix type errors

--- src/lib/recentlyviewed.ts
+++ src/lib/recentlyviewed.ts
@@ -2,18 +2,15 @@ import { User } from "~/server/api/routers/github";
 
 const RECENTLY_VIEWED_KEY = "recentlyViewedUsers";
 
-export function getRecentlyViewedUsers() {
+export function getRecentlyViewedUsers(): User[] {
   if (typeof window === "undefined") return [];
   const storedUsers = localStorage.getItem(RECENTLY_VIEWED_KEY);
-  console.log('walo');
-  return storedUsers ? JSON.parse(storedUsers) : [];
+  return storedUsers ? (JSON.parse(storedUsers) as User[]) : [];
 }
 
-export async function addRecentlyViewedUser(user: User) {
+export async function addRecentlyViewedUser(user: User): Promise<void> {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log(typeof window);
   if (typeof window !== "undefined") {
-    console.log('shit');
     const users = getRecentlyViewedUsers();
     const existingUser = users.find((u: User) => u.id === user.id);
     if (!existingUser) {
