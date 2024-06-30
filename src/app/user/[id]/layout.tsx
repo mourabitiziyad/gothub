f4b05277699c01e9f832dbf3ac712452cc64fddf
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-06-30 22:15:26 +0100

    ✨ feat: integrate header with generic layout

---
+++ src/app/user/[id]/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "~/components/Header";
+
+export default function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { id: string };
+}) {
+  return (
+    <div>
+      <Header params={params} />
+      {children}
+    </div>
+  );
+}
