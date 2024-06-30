84b779aed3f24cc7a8beb8a1bd9ce418de55c721
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-06-30 22:13:45 +0100

    ✨ feat(WIP): add route for userinfo

--- src/server/api/routers/github.ts
+++ src/server/api/routers/github.ts
@@ -18,6 +18,39 @@ export interface SearchResponse {
   };
 }
 
+export interface User {
+  id: string;
+  login: string;
+  avatarUrl: string;
+  url: string;
+  name?: string;
+  bio?: string;
+  location?: string;
+  company?: string;
+  email?: string;
+  followers: {
+    totalCount: number;
+  };
+  following: {
+    totalCount: number;
+  };
+  repositories: {
+    totalCount: number;
+  };
+  starredRepositories: {
+    totalCount: number;
+  };
+  gists: {
+    totalCount: number;
+  };
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface UserResponse {
+  user: User;
+}
+
 export const githubRouter = createTRPCRouter({
   getUsersList: publicProcedure
     .input(z.object({ text: z.string() }))
@@ -52,6 +85,51 @@ export const githubRouter = createTRPCRouter({
       } catch (error) {
         console.error("Error fetching GitHub users:", error);
         throw new Error("Failed to fetch GitHub users");
+      }
+    }),
+  getUserInfo: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const userQuery = `
+        query ($username: String!) {
+          user(login: $username) {
+            id
+            login
+            name
+            avatarUrl
+            url
+            bio
+            location
+            company
+            email
+            websiteUrl
+            followers {
+              totalCount
+            }
+            following {
+              totalCount
+            }
+            organizations(first: 100) {
+              nodes {
+                avatarUrl
+                login
+              }
+            }
+            achievements: contributionsCollection {
+              totalCommitContributions
+              restrictedContributionsCount
+            }
+          }
+        }
+      `;
+      try {
+        const result = await octokit.graphql<UserResponse>(userQuery, {
+          username: input.username,
+        });
+        return result.user;
+      } catch (error) {
+        console.error("Error fetching GitHub user:", error);
+        throw new Error("Failed to fetch GitHub user");
       }
     }),
 });
